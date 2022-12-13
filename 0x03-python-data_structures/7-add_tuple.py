7c5cc0a08b53dcd1cde0fea2da6002806e9da886
apholabby <afolabiadebola236@gmail.com>
2022-12-13 06:16:04 -0800

    new commit

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 7-add_tuple.py
 
-def add_tuple(tuple_a=(), tuple_b()):
+def add_tuple(tuple_a=(), tuple_b=()):
     """add two tuples"""
     if len(tuple_a) < 2:
         if len(tuple_a) == 0:
@@ -13,6 +13,5 @@ def add_tuple(tuple_a=(), tuple_b()):
             tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
-
 
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
